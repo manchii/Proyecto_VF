c95dd00c3a75be897ac85fedc15d7ae7c05bd974
manchii <kaleb.23415@gmail.com>
2018-10-27 12:12:50 -0600

    interfaces addded wishbone

--- sdr_ctrl/trunk/verif/tb_struct/interface.sv
+++ sdr_ctrl/trunk/verif/tb_struct/interface.sv
@@ -1,9 +1,21 @@
-interface intf(input logic clk,reset);
-  
+interface intf_wishbone 
+#(
+	parameter dw=32;
+)  
+(
+	input logic sys_clk,RESETN
+);
+
   //declaring the signals
-  logic       valid;
-  logic [3:0] a;
-  logic [3:0] b;
-  logic [6:0] c;
+	logic             wb_stb_i           ;
+	logic            wb_ack_o           ;
+	logic  [25:0]     wb_addr_i          ;
+	logic             wb_we_i            ; // 1 - Write, 0 - Read
+	logic  [dw-1:0]   wb_dat_i           ;
+	logic  [dw/8-1:0] wb_sel_i           ; // Byte enable
+	logic  [dw-1:0]  wb_dat_o           ;
+	logic             wb_cyc_i           ;
+	logic   [2:0]     wb_cti_i           ;
+
   
 endinterface
--- sdr_ctrl/trunk/verif/tb_struct/testbench.sv
+++ sdr_ctrl/trunk/verif/tb_struct/testbench.sv
@@ -13,35 +13,101 @@
 
 module tbench_top;
   
-  //clock and reset signal declaration
-  bit clk;
-  bit reset;
-  
-  //clock generation
-  always #5 clk = ~clk;
-  
-  //reset Generation
-  initial begin
-    reset = 1;
-    #5 reset =0;
-  end
-  
-  
+parameter P_SYS  = 10;     //    200MHz
+parameter P_SDR  = 20;     //    100MHz
+
+// General
+reg            RESETN;
+reg            sdram_clk;
+reg            sys_clk;
+
+initial sys_clk = 0;
+initial sdram_clk = 0;
+
+always #(P_SYS/2) sys_clk = !sys_clk;
+always #(P_SDR/2) sdram_clk = !sdram_clk;
+
+parameter      dw              = 32;  // data width
+parameter      tw              = 8;   // tag id width
+parameter      bl              = 5;   // burst_lenght_width 
+
+
+
+
+  // RAM
+
+   IS42VM16400K u_sdram16 (
+          .dq                 (Dq                 ), 
+          .addr               (sdr_addr[11:0]     ), 
+          .ba                 (sdr_ba             ), 
+          .clk                (sdram_clk_d        ), 
+          .cke                (sdr_cke            ), 
+          .csb                (sdr_cs_n           ), 
+          .rasb               (sdr_ras_n          ), 
+          .casb               (sdr_cas_n          ), 
+          .web                (sdr_we_n           ), 
+          .dqm                (sdr_dqm            )
+    );
+
+
+
   //creatinng instance of interface, inorder to connect DUT and testcase
-  intf i_intf(clk,reset);
+	intf_wishbone #(.dw(dw)) i_intf_wishbone(sys_clk,RESETN);
   
   //Testcase instance, interface handle is passed to test as an argument
-  test t1(i_intf);
+
+
+//  test t1(i_intf); --- when ready
   
+
+
+
   //DUT instance, interface signals are connected to the DUT ports
-  adder DUT (
-    .clk(i_intf.clk),
-    .reset(i_intf.reset),
-    .a(i_intf.a),
-    .b(i_intf.b),
-    .valid(i_intf.valid),
-    .c(i_intf.c)
-   );
+ sdrc_top #(.SDR_DW(16),.SDR_BW(2)) u_dut(
+	 .cfg_sdr_width      (2'b01              ), // 16 BIT SDRAM
+/* WISH BONE */
+          .wb_rst_i           (!i_intf_wishbone.RESETN            ),
+          .wb_clk_i           (i_intf_wishbone.sys_clk            ),
+
+          .wb_stb_i           (i_intf_wishbone.wb_stb_i           ),
+          .wb_ack_o           (i_intf_wishbone.wb_ack_o           ),
+          .wb_addr_i          (i_intf_wishbone.wb_addr_i          ),
+          .wb_we_i            (i_intf_wishbone.wb_we_i            ),
+          .wb_dat_i           (i_intf_wishbone.wb_dat_i           ),
+          .wb_sel_i           (i_intf_wishbone.wb_sel_i           ),
+          .wb_dat_o           (i_intf_wishbone.wb_dat_o           ),
+          .wb_cyc_i           (i_intf_wishbone.wb_cyc_i           ),
+          .wb_cti_i           (i_intf_wishbone.wb_cti_i           ), 
+
+/* Interface to SDRAMs */
+          .sdram_clk          (sdram_clk          ),
+          .sdram_resetn       (RESETN             ),
+          .sdr_cs_n           (sdr_cs_n           ),
+          .sdr_cke            (sdr_cke            ),
+          .sdr_ras_n          (sdr_ras_n          ),
+          .sdr_cas_n          (sdr_cas_n          ),
+          .sdr_we_n           (sdr_we_n           ),
+          .sdr_dqm            (sdr_dqm            ),
+          .sdr_ba             (sdr_ba             ),
+          .sdr_addr           (sdr_addr           ), 
+          .sdr_dq             (Dq                 ),
+
+    /* Parameters */
+          .sdr_init_done      (sdr_init_done      ),
+          .cfg_req_depth      (2'h3               ),	        //how many req. buffer should hold
+          .cfg_sdr_en         (1'b1               ),
+          .cfg_sdr_mode_reg   (13'h033            ),
+          .cfg_sdr_tras_d     (4'h4               ),
+          .cfg_sdr_trp_d      (4'h2               ),
+          .cfg_sdr_trcd_d     (4'h2               ),
+          .cfg_sdr_cas        (3'h3               ),
+          .cfg_sdr_trcar_d    (4'h7               ),
+          .cfg_sdr_twr_d      (4'h1               ),
+          .cfg_sdr_rfsh       (12'h100            ), // reduced from 12'hC35
+          .cfg_sdr_rfmax      (3'h6               )
+
+);
+
   
   //enabling the wave dump
   initial begin 
