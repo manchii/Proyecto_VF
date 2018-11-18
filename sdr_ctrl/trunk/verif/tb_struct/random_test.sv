90be6de2a89c2e20cb2d1c4b5fa5dd16219032ed
manchii <kaleb.23415@gmail.com>
2018-11-18 15:21:41 -0600

    Update random_test.sv

--- sdr_ctrl/trunk/verif/tb_struct/random_test.sv
+++ sdr_ctrl/trunk/verif/tb_struct/random_test.sv
@@ -9,7 +9,7 @@ program test(intf_wishbone i_intf_wishbone, intf_sdram16 i_intf_sdrwam16);
     env = new(i_intf_wishbone,i_intf_sdrwam16);
     //calling run of env, it interns calls generator and driver main tasks.
     env.pre_test();
-    this.test();
+    test();
     $finish;
   end
 
