6812d14d01aa389eef319ac7a114f1944c7d327b
manchii <kaleb.23415@gmail.com>
2018-11-24 05:29:41 -0600

    Update scoreboard.sv

--- sdr_ctrl/trunk/verif/tb_struct/scoreboard.sv
+++ sdr_ctrl/trunk/verif/tb_struct/scoreboard.sv
@@ -8,6 +8,7 @@
 
 
 //gets the packet from monitor, Generated the expected result and compares with the //actual result recived from Monitor
+typedef struct { int data[$], address, burst; } queue_ret ;
 
 class scoreboard;
    
@@ -17,16 +18,37 @@ class scoreboard;
 int dfifo[$]; // data fifo
 int afifo[$]; // address  fifo
 int bfifo[$]; // Burst Length fifo
-
+  
+queue_ret qfifo[$];
+  
 reg [31:0] ErrCnt;
 reg [31:0] StartAddr;  
 
+bit rand_order;
 
   //constructor
   function new();
 	this.ErrCnt = 0;
+    this.rand_order=0;
   endfunction
   
 
+  function push(queue_ret sample);
+    qfifo.push_back(sample);
+  endfunction
+  
+  function queue_ret pop();
+    int indx,i;
+    std::randomize(indx)
+    with {indx >= 0; indx < qfifo.size();};
+    $display("pop %d, size %d",indx, qfifo.size());
+    if(this.rand_order) begin 
+      pop = qfifo[indx];
+      qfifo.delete(indx);
+    end
+    else begin
+      pop = qfifo.pop_front();
+    end
+  endfunction  
   
 endclass
